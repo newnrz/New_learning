bca09421a1ffc29ac3448744103387fc499547dc
newnrz <nj.nirajjoshi@gmail.com>
2024-10-02 13:28:18 -0500

    sum calcu

---
+++ src/main/java/org/com/git/Sum.java
@@ -0,0 +1,12 @@
+package org.com.git;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+
+        System.out.println("the sum is: " + sum);
+    }
+}
