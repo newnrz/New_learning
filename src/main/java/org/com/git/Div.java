9642307a1201e0c7ea533212ddafb7ca6c1d3ce7
newnrz <nj.nirajjoshi@gmail.com>
2024-10-02 13:34:23 -0500

    div

---
+++ src/main/java/org/com/git/Div.java
@@ -0,0 +1,14 @@
+package org.com.git;
+
+public class Div {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int div = a-b;
+
+        System.out.println("the did is: "  +div);
+    }
+}
+
